560eaf7d5d26992bfd7903eae842de37df6648a3
veekshan <veekshanrai716@gmail.com>
2026-03-15 19:41:31 +0530

    DataBase Structure is created

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from expenses.models import Transaction
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['title', 'amount', 'transaction_type', 'date', 'category']
--- expenses/models.py
+++ expenses/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-TRANSACTION_TYPES = [
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
     ('Income', 'Income'),
     ('Expense', 'Expense')
-]
+    ]
 
-user = models.ForeignKey(User, on_delete=models.CASCADE)
-class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=265)
     amount = models.DecimalField( max_digits=5, decimal_places=2)
-    transaction_type = models.CharField(max_length=10, choices= TRANSACTION_TYPES)
+    transaction_type = models.CharField(max_length=10, 
+                                    choices=TRANSACTION_TYPES)
     date = models.DateField()
     category = models.CharField(max_length=255)
 
